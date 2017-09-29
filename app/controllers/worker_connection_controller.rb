0b02747c3db87bc36baede9b25e57b5ce1a8fe0b
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-09-29 14:05:21 -0400

    Minor update to worker_connection_controller.rb

--- app/controllers/worker_connection_controller.rb
+++ app/controllers/worker_connection_controller.rb
@@ -1,24 +1,14 @@
+# frozen_string_literal: true
+
 class WorkerConnectionController < WebsocketRails::BaseController
-
-  #def initialize_session
-  #end
-
-  #def authorize_public
-    #WebsocketRails[message['channel']].make_private
-  #end
-
   def connect
-    if worker
-      trigger_success
-    else
-      trigger_failure
-    end
+    worker ? trigger_success : trigger_failure
   end
 
   private
 
   def worker_user
-    @worker_user ||= User.joins(:api_keys).where(api_keys: {key: message["key"]}).first
+    @worker_user ||= User.joins(:api_keys).where(api_keys: { key: message["key"] }).first
   end
 
   def worker

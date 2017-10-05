683537aee207538b452297710c6726bcdfc03509
Adam Klen <adam.j.klen@gmail.com>
2017-10-05 11:05:10 -0400

    Added websockets better

--- service/process_wrapper.py
+++ service/process_wrapper.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import logging
+import json
 from subprocess import Popen
 import asyncio
 import signal
@@ -24,30 +25,51 @@ class ProcessWrapper():
         self.job_id=job_id
         self.command=command
 
-    async def start(self, loop, websocket):
-        logger.debug('Starting job: {}'.format(self.command))
-        process = await asyncio.create_subprocess_shell(
-                self.command,
-                stdin=asyncio.subprocess.PIPE,
-                stdout=asyncio.subprocess.PIPE)
-        job = asyncio.ensure_future(self.spawn_job(process, loop))
-        writer = asyncio.ensure_future(self.send_stdin(websocket, process.stdin))
-        reader = asyncio.ensure_future(self.read_stdout(websocket, process.stdout))
+    async def start(self, loop):
+        try:
+            # connects to websocket on host
+            async with websockets.connect(arguments.service_host) as websocket:
+                command = RegisterJob({"job_id": arguments.job_id, "api_key": arguments.token})
+                await websocket.send(str(command))
+                ack = await websocket.recv()
+                logger.debug("Successfully connected to server.")
+
+                logger.debug('Starting job: {}'.format(self.command))
+                process = await asyncio.create_subprocess_shell(
+                        self.command,
+                        stdin=asyncio.subprocess.PIPE,
+                        stdout=asyncio.subprocess.PIPE)
+                await asyncio.wait(
+                    [
+                        self.spawn_job(process, loop),
+                        self.send_stdin(websocket, process.stdin),
+                        self.read_stdout(websocket, process.stdout),
+                    ],
+                    loop=loop,
+                    return_when=asyncio.ALL_COMPLETED)
+        except Exception as e:
+            logger.debug("Error occured, terminating:", e)
+
 
     # TODO investigate connecting pipes https://docs.python.org/3/library/asyncio-eventloop.html#connect-pipes
     # also https://docs.python.org/3/library/asyncio-eventloop.html#watch-file-descriptors
     async def send_stdin(self, websocket, writer):
-        # TODO hook up to web socket
-        while 1:
-            ch = sys.stdin.read(1)
-            if not ch:
+        while websocket.open:
+            msg = await websocket.recv()
+            command, *payloads = json.loads(command)
+            logger.debug("Command:", command, "Payloads:", payloads)
+            if command == 'stop':
                 writer.close()
                 return
-            writer.write(ch.encode())
+            if command == 'stdin':
+                for payload in payloads:
+                    writer.write(payload)
+            else:
+                logger.error("Unexpected websocket command:", command)
 
     async def read_stdout(self, websocket, reader):
         # TODO hook up to web socket
-        while 1:
+        while websocket.open:
             ch = await reader.read(1)
             if not ch: return
             sys.stdout.write(ch.decode())
@@ -55,7 +77,6 @@ class ProcessWrapper():
     async def spawn_job(self, process, loop):
         code = await process.wait()
         self._job_terminated(code)
-        loop.stop()
 
     def ping_master(self):
         pass
@@ -71,20 +92,6 @@ class ProcessWrapper():
         Code to setup a job
         """
         pass
-
-async def start():
-    try:
-        # connects to websocket on host
-        async with websockets.connect(arguments.service_host) as websocket:
-            command = RegisterJob({"job_id": arguments.job_id, "api_key": arguments.token})
-            await websocket.send(str(command))
-            ack = await websocket.recv()
-            logger.debug("Successfully connected to server.")
-
-            asyncio.ensure_future(process_wrapper.start(loop, websocket), loop=loop)
-    except Exception as e:
-        logger.debug("Error occured, terminating:", e)
-
 
 if __name__ == "__main__":
     """
@@ -136,8 +143,9 @@ if __name__ == "__main__":
         command=arguments.command,
     )
     loop = asyncio.get_event_loop()
+
     try:
-        loop.run_until_complete(start())
+        loop.run_until_complete(process_wrapper.start(loop))
         loop.run_forever()
     finally:
         loop.close()

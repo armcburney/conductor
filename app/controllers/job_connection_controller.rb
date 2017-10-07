37623786ed219caaee8cdd58b84fcd53f768ca64
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-10-07 17:43:30 -0400

    Created job_connection_controller, and removed job related websocket logic from worker_connection_controller.

---
+++ app/controllers/job_connection_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class JobConnectionController < WebsocketRails::BaseController
+  def stdout
+    Rails.logger.info "Updated stdout for #{message['id']}, with stdout: #{message['stdout']}."
+    job.update(message.slice("stdout"))
+  end
+
+  def stderr
+    Rails.logger.info "Updated stderr for #{message['id']}, with stderr: #{message['stderr']}."
+    job.update(message.slice("stderr"))
+  end
+
+  def return_code
+    Rails.logger.info "Updated return code for #{message['id']}, with return code #{message['return_code']}."
+    job.update(message.slice("return_code"))
+  end
+
+  private
+
+  def job
+    @job ||= Job.find_by(id: message["id"])
+
+    # Triggers failure if it can't find the job
+    @job ? @job : trigger_failure
+  end
+end

ac5510988f76b0a1d3bacc7d0de57dfdbd433645
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-10-07 17:50:44 -0400

    Use safe navigator operator in job updates.

--- app/controllers/job_connection_controller.rb
+++ app/controllers/job_connection_controller.rb
@@ -3,17 +3,17 @@
 class JobConnectionController < WebsocketRails::BaseController
   def stdout
     Rails.logger.info "Updated stdout for #{message['id']}, with stdout: #{message['stdout']}."
-    job.update(message.slice("stdout"))
+    job&.update(message.slice("stdout"))
   end
 
   def stderr
     Rails.logger.info "Updated stderr for #{message['id']}, with stderr: #{message['stderr']}."
-    job.update(message.slice("stderr"))
+    job&.update(message.slice("stderr"))
   end
 
   def return_code
     Rails.logger.info "Updated return code for #{message['id']}, with return code #{message['return_code']}."
-    job.update(message.slice("return_code"))
+    job&.update(message.slice("return_code"))
   end
 
   private

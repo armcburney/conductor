b8cc3023bd47855cc46f954e94e42afa523f3eff
Andrew Codispoti <andrewcod749@gmail.com>
2017-10-07 00:56:57 -0400

    Added scripte to test with live heroku server.

---
+++ service/test_live.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python slave_master.py --hostname="AndrewMain" --service_host="ws://conductor-se464.herokuapp.com/websocket" --token="0c815c1ee6e71b015705c09b76a4761daea359d40d8383c38e0fc89753406a0b"
+

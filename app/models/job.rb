93e0aff4ff870b4c21e87d45bf8a9e52fc72f1a3
Dave Pagurek <dave@davepagurek.com>
2017-10-07 09:44:12 -0400

    Automatically subscribe client for updates, fix job id

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
   belongs_to :worker
   belongs_to :job_type
+
+  def request_json
+    # Send the id of the current job, plus the information from the job type
+    # needed to be able to spawn the job
+    job_type.as_json.merge(id: id)
+  end
 end

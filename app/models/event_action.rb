12d42770f18f8de5ff2cd00fe58cae826ca27f4e
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-10-08 15:19:32 -0400

    Updated models to fix rubocop errors and to provide structural conventions.

--- app/models/event_action.rb
+++ app/models/event_action.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class EventAction < ApplicationRecord
   belongs_to :event_receiver
   belongs_to :job_type, optional: true
-  validate :has_owned_job_type
+  validate :owned_job_type?
 
-  def has_owned_job_type
-    if job_type && job_type.user != event_action.user
-      errors.add(:job_type, "must be one of your jobs")
-    end
+  def owned_job_type?
+    return unless job_type && job_type.user != event_action.user
+    errors.add(:job_type, "must be one of your jobs")
   end
 end

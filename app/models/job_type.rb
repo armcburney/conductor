0d0a4163637b61ba3492f56c940c486afef159d2
Dave Pagurek <dave@davepagurek.com>
2017-10-12 18:50:20 -0400

    Reorder associations

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class JobType < ApplicationRecord
+  before_save :nullify_blanks
+
   belongs_to :user
   has_many :jobs
-
-  before_save :nullify_blanks
 
   private
 

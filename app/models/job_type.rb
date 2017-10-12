3c44e9ea0de6ef8d934db78439bcc151b9b9cb7f
Dave Pagurek <dave@davepagurek.com>
2017-10-12 19:04:00 -0400

    Hotfix null check

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -9,7 +9,7 @@ class JobType < ApplicationRecord
   private
 
   def nullify_blanks
-    environment_variables = "{}" if environment_variables.empty?
-    working_directory = nil if working_directory.empty?
+    environment_variables = "{}" if environment_variables&.empty?
+    working_directory = nil if working_directory&.empty?
   end
 end

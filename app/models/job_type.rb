458701c8eea609335e79e4c7293b62b8638ddfe8
Dave Pagurek <dave@davepagurek.com>
2017-10-12 18:42:01 -0400

    Use proper empty values for job info

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -3,4 +3,13 @@
 class JobType < ApplicationRecord
   belongs_to :user
   has_many :jobs
+
+  before_save :nullify_blanks
+
+  private
+
+  def nullify_blanks
+    environment_variables = "{}" if environment_variables.empty?
+    working_directory = nil if working_directory.empty?
+  end
 end

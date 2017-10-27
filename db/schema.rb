7a297ef422fc629cc982f1058025873789d721b3
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-10-27 12:53:11 -0400

    Added :type for event_receiver STI, and other event_receiver data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027163646) do
+ActiveRecord::Schema.define(version: 20171027164742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20171027163646) do
     t.datetime "updated_at",                  null: false
     t.integer  "user_id",                     null: false
     t.boolean  "triggered",   default: false, null: false
+    t.string   "type",                        null: false
+    t.text     "regex"
+    t.text     "stream"
+    t.integer  "return_code"
     t.index ["job_type_id"], name: "index_event_receivers_on_job_type_id", using: :btree
     t.index ["user_id"], name: "index_event_receivers_on_user_id", using: :btree
   end

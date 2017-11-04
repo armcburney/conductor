d0aff8725a6882b92c166ef89859b8da94c0f303
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-11-04 18:22:50 -0400

    Updated interval receiver and scheduled receiver validations.

--- app/models/interval_receiver.rb
+++ app/models/interval_receiver.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class IntervalReceiver < EventReceiver
-  def trigger_condition_met?(job)
+  validates :start_time, :interval, presence: true
+
+  # Only called from sidekiq job, at certain intervals
+  def trigger_condition_met?(_job)
+    true
   end
 end
--- app/models/scheduled_receiver.rb
+++ app/models/scheduled_receiver.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ScheduledReceiver < EventReceiver
+  validates :start_time, presence: true
+
   # Only called from sidekiq job, trigger condition will always be met
   def trigger_condition_met?(_job)
     true

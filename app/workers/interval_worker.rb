792dcdabff581179b21acd16c237283f85526588
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-11-07 10:48:48 -0500

    Run event actions through the EventReceiver instead of EventDispatcher in the IntervalWorker job.

--- app/workers/interval_worker.rb
+++ app/workers/interval_worker.rb
@@ -7,18 +7,11 @@ class IntervalWorker
   def perform(id)
     @receiver = Receiver.find_by(id)
     return unless receiver
-    dispatch_events
+    receiver.event_actions.each(&:run!)
     IntervalWorker.perform_at(start_time + (n * interval.seconds), id)
   end
 
   private
-
-  def dispatch_events
-    receiver.event_dispatches.each do |dispatcher|
-      dispatcher.dispatch!(receiver.user)
-      dispatcher.reset_trigger!
-    end
-  end
 
   def n
     ((Time.zone.now.to_i - start_time) / interval).ceil

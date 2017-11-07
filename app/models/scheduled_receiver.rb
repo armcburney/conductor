6f2322e2f5e3d9af28104bcaba95efc8df810801
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-11-07 00:43:17 -0500

    Added scheduled job worker.

--- app/models/scheduled_receiver.rb
+++ app/models/scheduled_receiver.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 class ScheduledReceiver < EventReceiver
+  # Callbacks
+  after_create :schedule_job
+
+  # Validations
   validates :start_time, presence: true
 
-  # Only called from sidekiq job, trigger condition will always be met
+  # Sidekiq job scheduled after_create => trigger condition will always be met
   def trigger_condition_met?(_job)
     true
+  end
+
+  private
+
+  def schedule_job
+    JobWorker.perform_at(start_time.from_now, id)
   end
 end

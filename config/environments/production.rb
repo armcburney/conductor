68635530a9a168840b7c5bcb270a087ca8f06743
Dave Pagurek <dave@davepagurek.com>
2017-11-14 20:31:28 -0500

    Change email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
+    enable_starttls_auto: auto,
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",

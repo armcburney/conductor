cf9ba37a28dce9901cdaa654842e179a3991e82b
Adam Klen <adam.j.klen@gmail.com>
2017-11-15 12:44:51 -0500

    Optionally take environment variable UPPER in write_test.py

--- service/jobs/write_test.py
+++ service/jobs/write_test.py
@@ -1,8 +1,16 @@
 import sys
 import time
+import os
 
-for i in range(10):
+# Optionally uses the environment variable UPPER to define an upper limit.
+upper = os.environ.get('UPPER')
+if upper:
+    upper = int(upper)
+else:
+    upper = 10
+print(upper)
+for i in range(upper):
     sys.stdout.write('stdout #%d\n' %i)
     sys.stderr.write('stderr #%d\n' %i)
-    time.sleep(0.5)
+    time.sleep(1)
 
